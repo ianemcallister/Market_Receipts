c183079bf493bc5d19053690d69c420044b5d033
Ian E McAllister <ianmcallistersubscriptions@gmail.com>
2016-07-26 00:17:17 -0700

    fix: server

--- server/mailcenter.js
+++ server/mailcenter.js
@@ -29,7 +29,7 @@ function sendEmail(sendTo, sendFrom, subject, body, attch) {
 	var smtpConfig = {
 		host: MAIL_HOST,
 		port: MAIL_PORT,
-		secure: false, // use SSL
+		secure: true, // use SSL
 		auth: {
 			user: MAIL_USER,
 			pass: MAIL_PASSWORD			
